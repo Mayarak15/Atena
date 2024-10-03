4c6ffb2c40bf9862d0668934fe9faf43687cafd5
Mayarak15 <mayarakelly0014@gmail.com>
2024-10-03 16:36:33 -0300

    3.0

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function entrar(){
     window.location.href = "paginainicial.html";
 }
+menuimg = document.querySelector("#img_menu");
+fecharmenu = document.querySelector("#fechar");
+menuu = document.querySelector("#menuhamburguer");
+
+function menu(){
+   
+    if(fecharmenu.style.display = "none"){
+        fecharmenu.style.display  = "block";
+        menuimg.style.display  = "none";
+        menuu.style.display = "block"
+}
+}
+function menufechar(){
+  
+if(fecharmenu.style.display = "block"){
+    fecharmenu.style.display  = "none";
+    menuimg.style.display  = "block";
+    
+        menuu.style.display = "none"
+}
+}
