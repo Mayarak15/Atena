d2012b9638370e478ed53fc571daf6c12713e89f
Mayarak15 <mayarakelly0014@gmail.com>
2024-10-09 16:15:45 -0300

    3.0

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
 function entrar(){
     window.location.href = "paginainicial.html";
 }
-menuimg = document.querySelector("#img_menu");
-fecharmenu = document.querySelector("#fechar");
-menuu = document.querySelector("#menuhamburguer");
 
-function menu(){
-   
-    if(fecharmenu.style.display = "none"){
-        fecharmenu.style.display  = "block";
-        menuimg.style.display  = "none";
-        menuu.style.display = "block"
+menu = document.querySelector("#opc_menu")
+fechar = document.querySelector("#fecharmenu")
+
+
+function embloco(){
+    menu.style.right = "0";
+    fechar = document.querySelector("#fecharmenu").style.display = "block"
+    abrir = document.querySelector("#abrirmenu").style.display = "none"
 }
+function invi(){
+    menu.style.right = "-100%";
+    abrir = document.querySelector("#abrirmenu").style.display = "block"
+    fechar = document.querySelector("#fecharmenu").style.display = "none"
 }
-function menufechar(){
-  
-if(fecharmenu.style.display = "block"){
-    fecharmenu.style.display  = "none";
-    menuimg.style.display  = "block";
-    
-        menuu.style.display = "none"
+
+menuperfil = document.querySelector("#menuperfil");
+
+function fecharmenuperfil(){
+    menuperfil.style.bottom = "-100%"
 }
+function menuperrfil(){
+    menuperfil.style.bottom = "0"
+}
+
+function grupo(){
+    window.location.href = "grupo.html";
+}
+
+definirtema = document.querySelector("#definirtema");
+imagtema = document.querySelector("#materiaescolhida_img");
+humtema = document.querySelector("#materiaescolhida_h1");
+
+function voltartema(){
+    definirtema.style.bottom = "-100%";
+}
+
+function humanas(){
+    definirtema.style.bottom = "0";
+    imagtema.src = "./imagens/humano.png";
+    humtema.innerHTML = "Ciências <br> Humanas"
+}
+function natureza(){
+    definirtema.style.bottom = "0";
+    imagtema.src = "./imagens/natureza.png";
+    humtema.innerHTML = "Ciências da<br> Natureza"
+}
+function linguagens(){
+    definirtema.style.bottom = "0";
+    imagtema.src = "./imagens/redacao.png";
+    humtema.innerHTML = "Linguagens"
+}
+function matematica(){
+    definirtema.style.bottom = "0";
+    imagtema.src = "./imagens/matematica.png";
+    humtema.innerHTML = "Matemática"
+}
+function tecnologia(){
+    definirtema.style.bottom = "0";
+    imagtema.src = "./imagens/tecnologia.png";
+    humtema.innerHTML = "Tecnologia"
 }
